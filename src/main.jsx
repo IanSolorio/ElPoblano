ad15ce9403e1850cede681578a03d2321d190fc6
IanSolorio <isolorio22@unamad.edu.pe>
2026-01-10 16:37:05 -0500

    Configuración de BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/ElPoblano">
       <App />
     </BrowserRouter>
   </React.StrictMode>
